70927612b599b60b4a000ac35ecbeebf676777cc
madrogalo <youn4ik@gmail.com>
2020-09-21 23:10:46 +0300

    add components

---
+++ src/components/chatWrap.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+function ChatWrap() {
+    return (
+        <div className="contacts-wrap">
+            <div className="contact">
+                <div className="contact-user">
+                    <div className="contact-user-foto">
+                        <a href=""><img src="" alt="" /></a>
+                    </div>
+                    <div className="contact-user-info">
+                        <div className="contact-user-top">
+                            <a href="" className="contact-user-name">
+                                john_snow
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-user active">
+                    <div className="contact-user-foto">
+                        <a href=""><img src="" alt="" /></a>
+                    </div>
+                    <div className="contact-user-info">
+                        <div className="contact-user-top">
+                            <a href="" className="contact-user-name">
+                                martin
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-user">
+                    <div className="contact-user-foto">
+                        <a href=""><img src="" alt="" /></a>
+                    </div>
+                    <div className="contact-user-info">
+                        <div className="contact-user-top">
+                            <a href="" className="contact-user-name">
+                                sherlock
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-user">
+                    <div className="contact-user-foto">
+                        <a href=""><img src="" alt="" /></a>
+                    </div>
+                    <div className="contact-user-info">
+                        <div className="contact-user-top">
+                            <a href="" className="contact-user-name">
+                                monica
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="contact-user">
+                    <div className="contact-user-foto">
+                        <a href=""><img src="" alt="" /></a>
+                    </div>
+                    <div className="contact-user-info">
+                        <div className="contact-user-top">
+                            <a href="" className="contact-user-name">
+                                dallas
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChatWrap;
